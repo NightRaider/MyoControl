a9471c5d8fa545e8702cd6bf55f577a3a43f5d61
NightRaider <Jeff.Fan.He@gmail.com>
2016-08-08 20:45:03 +0800

    Pitch for forward and backward movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,26 +36,30 @@ public class PlayerController : MonoBehaviour
                 Vector3 referenceZeroRoll = computeZeroRollVector(myo.transform.forward);
                 _referenceRoll = rollFromZero(referenceZeroRoll, myo.transform.forward, myo.transform.up);
 
-                
-
+                Vector3 referenceZeroPitch = computeZeroPitchVector(myo.transform.right);
+                _referencePitch = pitchFromZero(referenceZeroPitch, myo.transform.right, myo.transform.forward);
             }
         }
 
         Vector3 zeroRoll = computeZeroRollVector(myo.transform.forward);
         float roll = rollFromZero(zeroRoll, myo.transform.forward, myo.transform.up);
         float relativeRoll = normalizeAngle(roll - _referenceRoll);
-        
+
+        Vector3 zeroPitch = computeZeroPitchVector(myo.transform.right);
+        float pitch = rollFromZero(zeroPitch, myo.transform.right, myo.transform.forward);
+        float relativePitch = normalizeAngle(pitch- _referencePitch);
+
         if (thalmicMyo.pose == _lastPose && _lastPose == Pose.Fist)
         {
 
 
 
-            float moveHorizontal = -relativeRoll;
-            float moveVertical = Input.GetAxis("Vertical");
+            float moveHorizontal = relativeRoll;
+            float moveVertical = relativePitch;
 
-            Vector3 movement = new Vector3(moveHorizontal * factor, 0.0f, moveVertical);
+            Vector3 movement = new Vector3(moveHorizontal , 0.0f, moveVertical);
 
-            rb.AddForce(movement * speed);
+            rb.AddForce(-movement * speed);
         }
 
     }
@@ -80,16 +84,45 @@ public class PlayerController : MonoBehaviour
         return sign * Mathf.Rad2Deg * Mathf.Acos(cosine);
     }
 
+    float pitchFromZero(Vector3 zeroPitch, Vector3 right, Vector3 forward)
+    {
+        // The cosine of the angle between the up vector and the zero roll vector. Since both are
+        // orthogonal to the forward vector, this tells us how far the Myo has been turned around the
+        // forward axis relative to the zero roll vector, but we need to determine separately whether the
+        // Myo has been rolled clockwise or counterclockwise.
+        float cosine = Vector3.Dot(forward, zeroPitch);
+
+        // To determine the sign of the roll, we take the cross product of the up vector and the zero
+        // roll vector. This cross product will either be the same or opposite direction as the forward
+        // vector depending on whether up is clockwise or counter-clockwise from zero roll.
+        // Thus the sign of the dot product of forward and it yields the sign of our roll value.
+        Vector3 cp = Vector3.Cross(forward, zeroPitch);
+        float directionCosine = Vector3.Dot(right, cp);
+        float sign = directionCosine < 0.0f ? 1.0f : -1.0f;
+
+        // Return the angle of roll (in degrees) from the cosine and the sign.
+        return sign * Mathf.Rad2Deg * Mathf.Acos(cosine);
+    }
+
     // Compute a vector that points perpendicular to the forward direction,
     // minimizing angular distance from world up (positive Y axis).
     // This represents the direction of no rotation about its forward axis.
-    Vector3 computeZeroRollVector(Vector3 forward)
+    Vector3 computeZeroRollVector(Vector3 forward)  //axis that doesn't change
     {
-        Vector3 antigravity = Vector3.up;
+        Vector3 antigravity = Vector3.up;   //reference axis that rotate about, make angle with
         Vector3 m = Vector3.Cross(myo.transform.forward, antigravity);
         Vector3 roll = Vector3.Cross(m, myo.transform.forward);
 
         return roll.normalized;
+    }
+
+    Vector3 computeZeroPitchVector(Vector3 right)
+    {
+        Vector3 globalforward = Vector3.forward;
+        Vector3 m = Vector3.Cross(myo.transform.right, globalforward);
+        Vector3 pitch = Vector3.Cross(m, myo.transform.right);
+
+        return pitch.normalized;
     }
 
     // Adjust the provided angle to be within a -180 to 180.
