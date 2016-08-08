f02894d1c88a898440400d497836a8f92dfd8354
NiteRaider <Jeff.Fan.He@gmail.com>
2016-08-08 14:19:04 +0800

    Adding existing project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed;
+
+    public GameObject myo;
+    private Pose _lastPose = Pose.Unknown; 
+    
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
+
+        if (thalmicMyo.pose != _lastPose)
+        {
+            _lastPose = thalmicMyo.pose;
+
+        }
+
+        if (thalmicMyo.pose == _lastPose && _lastPose == Pose.Fist)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+            rb.AddForce(movement * speed);
+        }
+
+    }
+
+    float rollFromZero(Vector3 zeroRoll, Vector3 forward, Vector3 up)
+    {
+        // The cosine of the angle between the up vector and the zero roll vector. Since both are
+        // orthogonal to the forward vector, this tells us how far the Myo has been turned around the
+        // forward axis relative to the zero roll vector, but we need to determine separately whether the
+        // Myo has been rolled clockwise or counterclockwise.
+        float cosine = Vector3.Dot(up, zeroRoll);
+
+        // To determine the sign of the roll, we take the cross product of the up vector and the zero
+        // roll vector. This cross product will either be the same or opposite direction as the forward
+        // vector depending on whether up is clockwise or counter-clockwise from zero roll.
+        // Thus the sign of the dot product of forward and it yields the sign of our roll value.
+        Vector3 cp = Vector3.Cross(up, zeroRoll);
+        float directionCosine = Vector3.Dot(forward, cp);
+        float sign = directionCosine < 0.0f ? 1.0f : -1.0f;
+
+        // Return the angle of roll (in degrees) from the cosine and the sign.
+        return sign * Mathf.Rad2Deg * Mathf.Acos(cosine);
+    }
+
+    // Compute a vector that points perpendicular to the forward direction,
+    // minimizing angular distance from world up (positive Y axis).
+    // This represents the direction of no rotation about its forward axis.
+    Vector3 computeZeroRollVector(Vector3 forward)
+    {
+        Vector3 antigravity = Vector3.up;
+        Vector3 m = Vector3.Cross(myo.transform.forward, antigravity);
+        Vector3 roll = Vector3.Cross(m, myo.transform.forward);
+
+        return roll.normalized;
+    }
+
+    // Adjust the provided angle to be within a -180 to 180.
+    float normalizeAngle(float angle)
+    {
+        if (angle > 180.0f)
+        {
+            return angle - 360.0f;
+        }
+        if (angle < -180.0f)
+        {
+            return angle + 360.0f;
+        }
+        return angle;
+    }
+}
